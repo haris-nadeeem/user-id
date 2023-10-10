30eb505e24e18c0c6bbcb33511c628944783b530
haris-nadeeem <147490011+haris-nadeeem@users.noreply.github.com>
2023-10-10 22:27:34 +0500

    Add files via upload

---
+++ natural number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int a;
+	printf("enter a number\n");
+	scanf("%d",&a);
+	if(a>0){
+		printf("number is natural");
+	}
+	else{printf("number is not natural");
+	}
+	return 0;
+}
